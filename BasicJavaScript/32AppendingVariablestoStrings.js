d6fbb4022923084e22f1b5993a7fafe801df30f2
Vgyand <gyandzhunczevv@gmail.com>
2022-03-18 23:23:09 +0300

    32AppendingVariablestoStrings

---
+++ BasicJavaScript/32AppendingVariablestoStrings.js
@@ -0,0 +1,5 @@
+//Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+// Change code below this line
+const someAdjective = "asd";
+let myStr = "Learning to code is ";
+myStr += someAdjective
