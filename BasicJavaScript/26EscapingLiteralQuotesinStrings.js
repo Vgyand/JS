85b020ba3643384fff69450d2a37eefe318d530e
Vgyand <gyandzhunczevv@gmail.com>
2022-03-18 23:06:17 +0300

    26EscapingLiteralQuotesinStrings

---
+++ BasicJavaScript/26EscapingLiteralQuotesinStrings.js
@@ -0,0 +1,2 @@
+//Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
