9399b6ffcfcb88a7071ce2c6b40a1d56e48504bc
Vgyand <gyandzhunczevv@gmail.com>
2022-03-18 23:24:04 +0300

    33FindtheLengthofaString

---
+++ BasicJavaScript/33FindtheLengthofaString.js
@@ -0,0 +1,7 @@
+//Use the .length property to count the number of characters in the lastName variable and assign it to lastNameLength.
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
